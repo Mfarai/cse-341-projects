ff6fb06dbd654c47c978e57875bc91436caed9e7
farai <fmugijima8@gmail.com>
2024-01-16 04:45:33 +0200

    connected mongo

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 
+const mongodb = require('./data/database');
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port,() => {console.log(`running on port ${port}`)});
+
+
+mongodb.initDb((err) => {
+    if (err){
+        console.log(err);
+    }
+    else{
+        app.listen(port,() => {console.log(`database is listening and node running on port ${port}`)});
+    }
+});
