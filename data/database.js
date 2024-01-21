bc83f8ea295e1a8515be0c604dbd9673d7b38045
farai <fmugijima8@gmail.com>
2024-01-21 21:54:33 +0200

    installed swagger

--- data/database.js
+++ data/database.js
@@ -1,35 +1,34 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const MongoClient = require('mongodb').MongoClient;
-
 
 let database;
 
-const initDb = (callback) =>{
-    if (database) {
-        console.log('db already initialized');
-        return callback(null), database;
-        }
-        MongoClient.connect(process.env.MONGODB_URL)
-            .then((client)=>{
-                database = client;
-                callback(null, database);
-            })
-            .catch((err)=> {
-                callback(err);
-            });
+const initDb = (callback) => {
+  if (database) {
+    console.log('db already initialized');
+    return callback(null, database);
+  }
+
+  MongoClient.connect(process.env.MONGODB_URL)
+    .then((client) => {
+      database = client;
+      callback(null, database);
+    })
+    .catch((err) => {
+      callback(err);
+    });
 };
 
-const getDatabase =  () => {
-    if (!database) {
-        throw console.error("database not initialized")
-        }
-        return database;
+const getDatabase = () => {
+  if (!database) {
+    throw new Error("database not initialized");
+  }
+  return database;
 };
 
 module.exports = {
-    initDb,
-    getDatabase
+  initDb,
+  getDatabase,
 };
--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const mongodb = require('./data/database');
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 app.use('/', require('./routes'));
 
-
-
 mongodb.initDb((err) => {
-    if (err){
+    if (err) {
         console.log(err);
+        process.exit(1); // exit the process if there is an error
+    } else {
+        app.listen(port, () => {
+            console.log(`database is listening and node running on port ${port}`);
+        }).on('error', (err) => {
+            console.log(`Error starting server: ${err}`);
+            process.exit(1); // exit the process if there is an error
+        });
     }
-    else{
-        app.listen(port,() => {console.log(`database is listening and node running on port ${port}`)});
-    }
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+    mongodb.closeDb(() => {
+        console.log('Database connection closed on app termination');
+        process.exit(0);
+    });
 });
